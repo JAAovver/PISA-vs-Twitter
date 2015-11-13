52134c0c3641bddb5227bd173dd5f13edbc3944a
Jacob Mower <JAAovver@users.noreply.github.com>
2015-11-13 09:17:39 -0500

    actually use dataframes
    reads in total tweets, pisa tweets, and pisa scores - no need to manually manipulate anymore

--- PISAvsTwitter.py
+++ PISAvsTwitter.py
@@ -6,70 +6,65 @@ from scipy import stats
 import numpy as np
 import math
 
-read_file = 'pisa_words2.txt'
-tweets_data_path = read_file
-# tweets_data_path = 'all_words3.txt'
+def JsonToList(readfile):
+	tweets_data = []
+	tweets_file = open(readfile, "r")
+	for line in tweets_file:
+		try:
+			tweet = json.loads(line) #json attribute-value pairs for each tweet
+		except: #takes all kinds
+			continue
+		if ('text' and 'country' and 'place') in tweet: #make sure there's a country identifier and text
+			tweets_data.append(tweet)
+	return tweets_data
 
-tweets_data = []
-tweets_file = open(tweets_data_path, "r")
-for line in tweets_file:
-    try:
-        tweet = json.loads(line) #json attribute-value pairs for each tweet
-    except:
-        continue
-    if ('text' and 'country' and 'place') in tweet: #make sure there's a country identifier and text
-        tweets_data.append(tweet)
+def main():
+	pisa_frame = pd.read_csv('pisa_scores.csv',index_col=0) #countries' pisa scores
+	AllishTweets = JsonToList('all_words3.txt') # tweet volume for normalization
+	PisaTweets = JsonToList('pisa_words2.txt') # 'intellectual' tweets
 
-tweets = pd.DataFrame()
+	PisaTabled = pd.DataFrame()
+	AllishTabled = pd.DataFrame()
+    
+    # look at country of origin
+	PisaTabled['country'] = map(lambda tweet: tweet['place']['country'] if tweet['place'] != None else None, PisaTweets)
+	AllishTabled['country'] = map(lambda tweet: tweet['place']['country'] if tweet['place'] != None else None, AllishTweets)
 
-tweets['country'] = map(lambda tweet: tweet['place']['country'] if tweet['place'] != None else None, tweets_data)
+    # how many tweets? Make a series of that for the different countries
+	PisaByCtry = PisaTabled['country'].value_counts()
+	AllishByCtry = AllishTabled['country'].value_counts()
+	
+	# take the counts, ""join"" as dataframe and also add in pisa scors
+	PisaEtAll = pd.concat([PisaByCtry,AllishByCtry,pisa_frame],axis=1, join='inner')
 
-tweets_by_country = tweets['country'].value_counts()
-print tweets_by_country[:20]
-normalizers = [2258.0, 446.0,1,99.0,169.0,53.0,172.0,1,1,34.0,52.0] #representation of all tweets - pisa_words2.txt/all_words3.txt
-tweet_score = [100*tweets_by_country[i]/normalizers[i] for i in range(11)]
-print tweets_by_country[:20]
+	pisa_score = PisaEtAll['score'] # read from that dataframe column
+	tweet_score = PisaEtAll[0]/PisaEtAll[1] # calculate frequency of intellectual tweets
 
-del tweet_score[2] #no pisa score for phillipines
-del tweet_score[6] #no count score for spain
-del tweet_score[6] #no pisa score for india
+    # prepare for scipy linregress
+	X = np.array(pisa_score)
+	Y = np.array(tweet_score)	
+	slope, intercept, r_value, p_value, std_err = stats.linregress(X,Y)
+	R2 = r_value**2
+	print 
+	print 'The slope is: ', slope
+	print 'The intercept is: ', intercept
+	print 'The r-squared value is: ', R2
+	# z = np.polyfit(X,Y,2)
+	# f = np.poly1d(z)
+	# xplot = np.linspace(X[0],X[-1],50)
+	# yplot = f(x_plot)
 
-pisa_score = [481,494,518,391,495,421,375,485] #PISA scores from http://www.oecd.org/pisa/keyfindings/pisa-2012-results-overview.pdf
+	xplot = range(600)
+	yplot = [intercept+slope*i for i in range(600)]
+	area = [PisaEtAll[1][i] for i in range(len(pisa_score))]
+	colors = np.linspace(0.1,1,len(pisa_score))
+	plt.scatter(pisa_score,tweet_score, s=np.array(area), c=colors, alpha=0.5)
+	plt.plot(xplot,yplot,linestyle='--',linewidth=2,color='r')
+	plt.xlabel('Raw PISA Score')
+	plt.ylabel('Percent of texts containing PISA-like words')
+	plt.title('Culture of intellectual discourse vs. PISA scores')
+	plt.axis([350, 525, 0, 1])
+	plt.show()
 
-X = np.array(pisa_score)
-Y = np.array(tweet_score)
-slope, intercept, r_value, p_value, std_err = stats.linregress(X,Y)
-R2 = r_value**2
-print 
-print 'The slope is: ', slope
-print 'The intercept is: ', intercept
-print 'The r-squared value is: ', R2
-
-
-xplot = range(600)
-yplot = [intercept+slope*i for i in range(600)]
-area = [normalizers[i] for i in range(8)]
-colors = np.array([0.1, 0.3, 0.5, 0.7, 0.9])
-colors = np.array([0.1, 0.3, 0.5, 0.7, 0.9, 0.2, 0.4, 0.6])
-plt.scatter(pisa_score,tweet_score, s=np.array(area), c=colors, alpha=0.5)
-plt.plot(xplot,yplot,linestyle='--',linewidth=2,color='r')
-plt.xlabel('Raw PISA Score')
-plt.ylabel('Percent of texts containing PISA-like word')
-plt.title('Culture of intellectual discourse vs. PISA scores')
-plt.axis([350, 525, 0, 75])
-plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+	main()
